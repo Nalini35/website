d0f15e72c3ad0801394f2085dbdc89bd2bc125eb
“Nalini35” <nalini@qaoncloud.com>
2022-06-09 15:48:18 +0530

    commit

--- cypress/integration/1-getting-started/test.spec.js
+++ cypress/integration/1-getting-started/test.spec.js
@@ -6,12 +6,12 @@ describe('My First Test', function ()
     
   cy.visit('https://www.flipkart.com/')
   cy.get('._3704LK').click()
-  cy.get('._3704LK').type('lipstick under 100')
+  cy.get('._3704LK').type('lipstick under 100 abcd')
   cy.get('.L0Z3Pu').click()
   cy.get('.JWMl0H > ._2ssEMF > ._3V8rao > span').click()
   cy.get('._5THWM1 > :nth-child(6)').click()
   cy.get('.exehdJ').click()
-  cy.get('[data-id="LSKGAGT9KB22GFGC"] > ._4ddWXP > .s1Q9rs').click()
+  
   
 
   
